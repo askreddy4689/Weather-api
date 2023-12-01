7c75806fd8cf15361319697a4bccd698812b4801
askreddy4689 <90843217+askreddy4689@users.noreply.github.com>
2023-12-01 11:49:45 +0530

    API

--- app.js
+++ app.js
@@ -1,29 +1,75 @@
 //jshint esversion:6
-const express=require("express");
-const https =require("https");
-const bodyparser=require("body-parser");
-const app=express();
-app.use(express.static("public"));
-app.use(bodyparser.urlencoded({extended:true}));
+const express = require('express');
+const https = require('https');
+const bodyparser = require('body-parser');
+const app = express();
+app.use(express.static('public'));
+app.use(bodyparser.urlencoded({ extended: true }));
 
-app.get("/",function(req,res){
- res.sendFile(__dirname+"/index.html");
+app.set('view engine', 'ejs');
+// var city = 'Madanapalle';
+// var temp = '';
+// var country = '';
+// var url = '';
+
+const sender = (city) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      https.get(
+        'https://api.openweathermap.org/data/2.5/weather?q=' +
+          city +
+          '&appid=03c28434b0c9b9a200d45bd7770e4821&units=metric',
+        (response) => {
+          response.on('data', async (data) => {
+            const wdata = await JSON.parse(data);
+            if (wdata.cod == 404) {
+              res.send('city not found');
+            } else {
+              var dataset = {
+                city: '',
+                temp: '',
+                country: '',
+                url: '',
+              };
+              dataset.temp = wdata.main.temp;
+              const icon = wdata.weather[0].icon;
+              dataset.country = wdata.sys.country;
+              dataset.city = wdata.name;
+              dataset.url =
+                'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+              resolve(dataset);
+            }
+          });
+        }
+      );
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+
+app.get('/', async (req, res) => {
+  const displaydata = await sender('madanapalle');
+
+  res.render('index', {
+    currcity: displaydata.city,
+    citytemp: displaydata.temp,
+    pic: displaydata.url,
+    currcountry: displaydata.country,
+  });
 });
-app.post("/", function(req,res){
-const city= req.body.cityname;
-//console.log(city);
-https.get("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=03c28434b0c9b9a200d45bd7770e4821&units=metric",function(response){
-   response.on("data",function(data){
-     const wdata=JSON.parse(data);
-      const temp=wdata.main.temp;
-      const icon=wdata.weather[0].icon;
-      const url="http://openweathermap.org/img/wn/"+icon+"@2x.png";
-      res.write("<h1>The temperature in "+city +" is "+temp+"degree celcius.</h1>");
-     res.write("<img src="+url+">");
-     res.send();
-    });
+
+app.post('/', async (req, res) => {
+  curcity = req.body.cityname;
+  const displaydata = await sender(curcity);
+
+  res.render('index', {
+    currcity: displaydata.city,
+    citytemp: displaydata.temp,
+    pic: displaydata.url,
+    currcountry: displaydata.country,
+  });
 });
-});
-app.listen(3000,function(){
- console.log("server is running on port 3000.");
+app.listen(3000, function () {
+  console.log('server is running on port 3000.');
 });
